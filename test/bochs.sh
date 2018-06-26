c4fb6e3cfb9e7189ff573170c6dc6b1de944361c
Georges Dupéron <georges.duperon@gmail.com>
2018-06-26 02:35:07 +0200

    Added bochs

--- test/bochs.sh
+++ test/bochs.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
 set -e
 
-bochs --help
+os_file="example-os/os.sh"
+
+bochsrc="$(tempfile)"
+cat > "$bochsrc" <<EOF
+floppya: 1_44=${os_file}, status=inserted
+boot: floppy
+EOF
+
+bochscontinue="$(tempfile)"
+echo "continue" > "$bochscontinue"
+
+bochs -qf "$bochsrc" < "$bochscontinue" &
+pid=$!
+runsikulix -r test/check-gradient.sikuli
+kill $pid
