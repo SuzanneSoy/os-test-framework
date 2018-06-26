8e65a81bfc7b706ced7612bf1f561cea24f41de2
Georges Dupéron <georges.duperon@gmail.com>
2018-06-26 02:07:58 +0200

    Use runsikuli from $PATH, so that the tests may be run outside of Travis

--- test/qemu-system-i386.sh
+++ test/qemu-system-i386.sh
@@ -5,5 +5,5 @@ os_file="example-os/os.sh"
 
 qemu-system-i386 -drive format=raw,file=${os_file},index=0,if=floppy &
 pid=$!
-~/sikulix/runsikulix -r test/check-gradient.sikuli
+runsikulix -r test/check-gradient.sikuli
 kill $pid
