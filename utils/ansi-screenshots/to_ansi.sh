29dc3d26cf10d8106d75c3bdefeb7a6837d1639a
Georges Dupéron <georges.duperon@gmail.com>
2018-06-29 23:41:57 +0200

    Attempt at slowing down the output even more, so that Travis finally stops corrupting the data.

--- utils/ansi-screenshots/to_ansi.sh
+++ utils/ansi-screenshots/to_ansi.sh
@@ -40,4 +40,4 @@ paste "$odd_lines_px" "$even_lines_px" \
  | tr '\t\n' '  ' \
  | fold -w $((width*6)) \
  | sed -r -e 's/([01])([0-7]) ([01])([0-7]) /[\1;3\2;4\4m▀/g' -e 's/$/[m/' \
- | if test "$CI" = "true" -a "$TRAVIS" = "true"; then while read ab; do echo "$ab"; sleep 0.1; done; else cat; echo; fi
+ | if test "$CI" = "true" -a "$TRAVIS" = "true"; then while IFS='' read -n 1 ab; do if test -z "$ab"; then echo; else echo -n "${ab}"; fi; sleep 0.001; done; else cat; echo; fi
