a42e96d7730a3783eef998965dea6b4f881ecec2
Georges Dupéron <georges.duperon@gmail.com>
2018-06-30 00:53:36 +0200

    Added extra newline with the new slowdown

--- utils/ansi-screenshots/to_ansi.sh
+++ utils/ansi-screenshots/to_ansi.sh
@@ -40,4 +40,5 @@ paste "$odd_lines_px" "$even_lines_px" \
  | tr '\t\n' '  ' \
  | fold -w $((width*6)) \
  | sed -r -e 's/([01])([0-7]) ([01])([0-7]) /[\1;3\2;4\4m▀/g' -e 's/$/[m/' \
- | if test "$CI" = "true" -a "$TRAVIS" = "true"; then while IFS=$'\n' read -n 11 ab; do if test "${#ab}" -ne 11; then echo "$ab"; else echo -n "$ab"; fi; sleep 0.01; done; else cat; echo; fi
+ | if test "$CI" = "true" -a "$TRAVIS" = "true"; then while IFS=$'\n' read -n 11 ab; do if test "${#ab}" -ne 11; then echo "$ab"; else echo -n "$ab"; fi; sleep 0.01; done; else cat; fi
+echo
