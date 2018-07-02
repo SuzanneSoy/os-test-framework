1c27435169ad8d2434d1d47928bc0dba21417325
Georges Dupéron <georges.duperon@gmail.com>
2018-07-03 00:00:32 +0200

    Changed the xvfb resolution so that the window title bar is visible, removed fluxbox's background picture

--- utils/gui-wrapper.sh
+++ utils/gui-wrapper.sh
@@ -1,2 +1,21 @@
 #!/bin/sh
-xvfb-run -a sh -c 'fluxbox 2>/dev/null & sleep 3; "$@"' utils/gui-wrapper.sh-subshell "$@"
+
+resolution="$1" # e.g. 800x600x24 (width x height x bits_per_pixel)
+shift           # the following arguments are the program to execute and its arguments
+
+bg="$(mktemp --suffix='bg.png')"
+fb_cfg="$(mktemp --suffix='fluxbox.cfg')"
+
+# Create solid black background
+convert -size 16x16 xc:black "$bg"
+
+# Create minimalist fluxbox configuration
+cat > "$fb_cfg" <<EOF
+background:	fullscreen
+background.pixmap:	$bg
+EOF
+
+xvfb-run -a --server-args="-screen 0 ${resolution}" sh -c 'fluxbox -rc '"$fb_cfg"' 2>/dev/null & sleep 3; "$@"' utils/gui-wrapper.sh-subshell "$@"
+
+# Cleanup
+rm "$bg" "$fb_cfg"
