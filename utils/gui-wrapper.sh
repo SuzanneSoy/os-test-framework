059fc30af17cbcc61959dc8a1e58ecc3b8eb41e2
Georges Dupéron <georges.duperon@gmail.com>
2018-07-04 01:30:21 +0200

    Save an animation to help with debugging, increase delay after fbsetbg

--- utils/gui-wrapper.sh
+++ utils/gui-wrapper.sh
@@ -6,11 +6,14 @@ resolution="$1" # e.g. 800x600x24 (width x height x bits_per_pixel)
 shift           # the following arguments are the program to execute and its arguments
 
 bg="$(mktemp --suffix='.png')"
+anim="$(mktemp -d)"
 
 # Create solid black background
 convert -size "$(echo "$resolution" | cut -d 'x' -f1-2)" tile:pattern:checkerboard "$bg"
 
-xvfb-run -a --server-args="-screen 0 ${resolution}" sh -c 'fluxbox 2>/dev/null & sleep 5; fbsetbg -f '"$bg"'; sleep 2; "$@"' utils/gui-wrapper.sh-subshell "$@"
+xvfb-run -a --server-args="-screen 0 ${resolution}" sh -c 'fluxbox 2>/dev/null & sleep 5; fbsetbg -f "'"$bg"'"; sleep 5; (for i in `seq 100`; do scrot "'"$anim"'/$(printf %03d.png $i)"; sleep 0.2; done) & "$@"' utils/gui-wrapper.sh-subshell "$@"
+
+convert $(ls "$anim"/*.png | sort) ./deploy-screenshots/anim.gif
 
 # Cleanup
 rm "$bg"
