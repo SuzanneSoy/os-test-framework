d5464b0227e80780896786228ee0b6c85c2f5659
Georges Dupéron <georges.duperon@gmail.com>
2018-07-03 21:39:08 +0200

    Use fbsetbg since the fluxbox version on Travis consistently overwrites configuration files with the defaults

--- utils/gui-wrapper.sh
+++ utils/gui-wrapper.sh
@@ -6,24 +6,11 @@ resolution="$1" # e.g. 800x600x24 (width x height x bits_per_pixel)
 shift           # the following arguments are the program to execute and its arguments
 
 bg="$(mktemp --suffix='.png')"
-fb_cfg="$(mktemp --suffix='fluxbox.cfg')"
-fb_sty="$(mktemp --suffix='fluxbox.style')"
 
 # Create solid black background
 convert -size "$(echo "$resolution" | cut -d 'x' -f1-2)" tile:pattern:checkerboard "$bg"
 
-# Create minimalist fluxbox configuration
-cat > "$fb_sty" <<EOF
-background:	fullscreen
-background.pixmap:	$bg
-EOF
-
-cat > "$fb_cfg" <<EOF
-session.styleFile: $fb_sty
-EOF
-
-
-xvfb-run -a --server-args="-screen 0 ${resolution}" sh -c 'fluxbox -rc '"$fb_cfg"' 2>/dev/null & sleep 3; "$@"' utils/gui-wrapper.sh-subshell "$@"
+xvfb-run -a --server-args="-screen 0 ${resolution}" sh -c 'fluxbox 2>/dev/null & sleep 5; fbsetbg -f '"$bg"'; "$@"' utils/gui-wrapper.sh-subshell "$@"
 
 # Cleanup
-rm "$bg" "$fb_cfg"
+rm "$bg"
