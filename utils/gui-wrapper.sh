7b5a320bc038b4b089c5c70541f3b795f244285d
Georges Dupéron <georges.duperon@gmail.com>
2018-07-07 18:50:11 +0200

    Cleanup temporary files, put twm session file in a temporary directory instead of ~

--- utils/gui-wrapper.sh
+++ utils/gui-wrapper.sh
@@ -7,12 +7,16 @@ shift           # the following arguments are the program to execute and its arg
 
 bg="$(mktemp --suffix='.xbm')"
 twm_cfg="$(mktemp --suffix='_twm.cfg')"
+twm_session_dir="$(mktemp -d)"
 anim="$(mktemp -d)"
 
-# Create solid black background
+# Create checkerboard background
+# Use +level-colors 'gray(192),gray(128)' to choose directly the colors.
+#   Since we're creating an .xbm for xsetroot, we'll use black and white
+#   and choose the colors with xsetroot.
 convert -size "$(echo "$resolution" | cut -d 'x' -f1-2)" \
         tile:pattern:checkerboard \
-        -auto-level +level-colors 'gray(192),gray(128)' \
+        -auto-level \
         "$bg"
 
 cat > "$twm_cfg" <<EOF
@@ -21,14 +25,14 @@ EOF
 
 # -fg chocolate -bg coral looks nice too :)
 echo "$bg $twm_cfg $anim"
-xvfb-run -a --server-args="-screen 0 ${resolution}" sh -c 'twm -f "'"$twm_cfg"'" & xsetroot -bitmap "'"$bg"'" -fg gray75 -bg gray50; sleep 1; utils/screenshots-loop.sh "'"$anim"'" & "$@"' utils/gui-wrapper.sh-subshell "$@"
+xvfb-run -a --server-args="-screen 0 ${resolution}" sh -c 'SM_SAVE_DIR="'"$twm_session_dir"'" twm -f "'"$twm_cfg"'" & xsetroot -bitmap "'"$bg"'" -fg gray75 -bg gray50; sleep 1; utils/screenshots-loop.sh "'"$anim"'" & "$@"' utils/gui-wrapper.sh-subshell "$@"
 
 touch "$anim/stop-screenshots"
 for i in `seq 60`; do if test -e "$anim/anim-done"; then break; fi; sleep 1; done
 if test -e "$anim/anim.gif"; then
   mv "$anim/anim.gif" "./deploy-screenshots/$(basename "$1" .sh)-anim.gif"
 fi
-cp "$bg" "./deploy-screenshots/$(basename "$1" .sh)-bg-$(basename "$bg")"
 
 # Cleanup
-rm "$bg"
+rm -- "$bg" "$twm_cfg"
+rm -r -- "$twm_session_dir" "$anim"
