47a7cf82b1d27d833712750f7802de051947dceb
Georges Dupéron <georges.duperon@gmail.com>
2018-07-08 23:43:55 +0200

    There seems to sometimes be an issue when creating the animation with the parallel build. Added more debug output, increased timeout.

--- utils/gui-wrapper.sh
+++ utils/gui-wrapper.sh
@@ -23,12 +23,14 @@ cat > "$twm_cfg" <<EOF
 RandomPlacement
 EOF
 
+echo "$bg $twm_cfg $anim $resolution $@"
 # -fg chocolate -bg coral looks nice too :)
-echo "$bg $twm_cfg $anim"
 xvfb-run -a --server-args="-screen 0 ${resolution}" sh -c 'sleep 2; SM_SAVE_DIR="'"$twm_session_dir"'" twm -f "'"$twm_cfg"'" & sleep 1 && xsetroot -bitmap "'"$bg"'" -fg gray75 -bg gray50 && sleep 1 && utils/screenshots-loop.sh "'"$anim"'" & "$@"' utils/gui-wrapper.sh-subshell "$@"
 
 touch "$anim/stop-screenshots"
-for i in `seq 60`; do if test -e "$anim/anim-done"; then break; fi; sleep 1; done
+anim_done=false
+for i in `seq 300`; do if test -e "$anim/anim-done"; then anim_done=true; break; fi; sleep 1; done
+if $anim_done; then echo "anim: done ($*)"; else echo "anim: timeout ($*)"; fi
 if test -e "$anim/anim.gif"; then
   mv "$anim/anim.gif" "./deploy-screenshots/$(basename "$1" .sh)-anim.gif"
 fi
