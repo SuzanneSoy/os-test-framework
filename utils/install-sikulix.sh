411e0eb66697f24af847cfe185e0b49b4f28b0f5
Georges Dupéron <georges.duperon@gmail.com>
2018-07-29 18:08:46 +0200

    appveyor: install sikulix

--- utils/install-sikulix.sh
+++ utils/install-sikulix.sh
@@ -4,6 +4,6 @@ set -e
 
 # The sikuli-ide packaged with ubuntu 16.04 does not seem to work correctly: missing dependencies, some dependencies are too recent, …
 mkdir ~/sikulix/
-wget https://launchpad.net/sikuli/sikulix/1.1.3/+download/sikulixsetup-1.1.3.jar -O ~/sikulix/sikulixsetup-1.1.3.jar
+curl -o ~/sikulix/sikulixsetup-1.1.3.jar https://launchpad.net/sikuli/sikulix/1.1.3/+download/sikulixsetup-1.1.3.jar
 (cd ~/sikulix && java -jar sikulixsetup-1.1.3.jar options 1 1.1)
 echo 'export PATH="$HOME/sikulix/:$PATH"'
